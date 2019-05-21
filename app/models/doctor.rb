6686e9e7f863aed22c2afab37c233dceec8feb09
Patrick Lamoureux <patrick@blackcedar.com>
2019-05-21 14:23:44 -0600

    New Doc form works

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,9 @@
 class Doctor < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_many :users, through: :appointments
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+  
 end
